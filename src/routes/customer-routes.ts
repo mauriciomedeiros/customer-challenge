e1ad3c7c5e997fa27775be06815ed2fcfc806e7f
Mauricio Medeiros <mauriciomedeiros.05@gmail.com>
2020-10-12 18:42:38 -0300

    add authorization in routes

--- src/routes/customer-routes.ts
+++ src/routes/customer-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { CustomerController } from "@src/controllers/customer-controller";
+import { AuthMiddleware } from './middlewares/auth-middleware';
 
 export class CustomerRoutes {
   public static routes(routes: Router): void {
-    routes.get('/challenge/customer/:id', CustomerController.getByID);
-    routes.post('/challenge/customer', CustomerController.create);
-    routes.put('/challenge/customer', CustomerController.update);
-    routes.delete('/challenge/customer/:id', CustomerController.delete);
+    routes.get('/challenge/customer/:id', AuthMiddleware.decodeToken, CustomerController.getByID);
+    routes.post('/challenge/customer', AuthMiddleware.decodeToken, CustomerController.create);
+    routes.put('/challenge/customer', AuthMiddleware.decodeToken, CustomerController.update);
+    routes.delete('/challenge/customer/:id', AuthMiddleware.decodeToken, CustomerController.delete);
   }
 }
--- src/routes/favorite-product-routes.ts
+++ src/routes/favorite-product-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { FavoriteProductController } from '@src/controllers/favorite-product-controller';
+import { AuthMiddleware } from './middlewares/auth-middleware';
 
 
 export class FavoriteProductRoutes {
   public static routes(routes: Router): void {
-    routes.get('/challenge/customer/:id/favorite/product', FavoriteProductController.getByCustomer);
-    routes.post('/challenge/customer/:id/favorite/product', FavoriteProductController.add);
-    routes.delete('/challenge/customer/:id/favorite/product/:idProduct', FavoriteProductController.remove);
+    routes.get('/challenge/customer/:id/favorite/product', AuthMiddleware.decodeToken, FavoriteProductController.getByCustomer);
+    routes.post('/challenge/customer/:id/favorite/product', AuthMiddleware.decodeToken, FavoriteProductController.add);
+    routes.delete('/challenge/customer/:id/favorite/product/:idProduct', AuthMiddleware.decodeToken, FavoriteProductController.remove);
   }
 }
